6d8298d1ad4027a0090629ad8a4a195e3a17fe6d
Obadia Gilbert <gilbertobadia830@gmail.com>
2022-09-10 06:32:21 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: print number from 1 to 10
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n > 10; n++)
+	{
+		printf(n);
+	}
+	printf('\n');
+	return (0);
+}
