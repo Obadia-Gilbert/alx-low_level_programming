66b2bffbbfef9e271fa22699b197a9dd912f3cf8
Obadia Gilbert <gilbertobadia830@gmail.com>
2022-09-10 08:20:38 -0700

    10-print_comb2.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: print two number combinations
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+	
+	for (d = 0; d < 100; d++)
+	{
+		putchar((d / 10) + '0');
+		putchar((d % 10) + '0');
+		if (d != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
