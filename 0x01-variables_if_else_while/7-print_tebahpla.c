dd76c2fa12862f61dd8ef2aecb7f4d8a363f6caf
Obadia Gilbert <gilbertobadia830@gmail.com>
2022-09-10 07:31:07 -0700

    7-print_tepahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: print alphabet in reverse
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'z'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
