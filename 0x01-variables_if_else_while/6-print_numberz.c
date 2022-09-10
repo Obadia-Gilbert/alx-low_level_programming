e8ea3c8c16a3eec1e30f959598ce21b81ed7c4eb
Obadia Gilbert <gilbertobadia830@gmail.com>
2022-09-10 06:53:25 -0700

    print numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: print digits using putchar 1-9
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int d;
+
+	for (d = '0'; d <= '10'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
+}
