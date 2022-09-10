72b8d2187a7d5e025b1939be6ac609a729f38644
Obadia Gilbert <gilbertobadia830@gmail.com>
2022-09-10 05:25:58 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: Print Characters using putchar
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = a; c <= z; c++)
+	{
+		putchar("%c\n", c);
+	}
+	return (0);
+}
