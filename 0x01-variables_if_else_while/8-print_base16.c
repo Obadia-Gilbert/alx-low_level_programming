0f3ea3c2dc63862963aacf97e125ead20c909b41
Obadia Gilbert <gilbertobadia830@gmail.com>
2022-09-10 07:50:55 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Enty Point
+ * Description: print 
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int d;
+	char c;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+		putchar(d);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
