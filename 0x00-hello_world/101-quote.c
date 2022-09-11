ed7de66deb2aa730a8006534bf699688bad0178c
Obadia Gilbert <gilbertobadia830@gmail.com>
2022-09-11 12:42:31 -0700

    advance hello world c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistdio.h>
 /**
  * main - Entry point
  * Description: write 2 that piece of art is useful ..
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
